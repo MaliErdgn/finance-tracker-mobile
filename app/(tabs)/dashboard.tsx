830ad3d3d0b2b3b456426a035877a61929c68f30
MaliErdgn <malierdgnn@gmail.com>
2024-10-08 21:39:51 +0300

    added other endpoints and funcionalities (see desc) - Aded the tags, methods, types, categories endpoints - Added types and use states of the mentioned variables and fetched - created functions to assign the variables' names depending on their ids. - Still styling not done

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -10,7 +10,7 @@ import {
   Platform,
   UIManager,
   FlatList,
-  Pressable
+  Pressable,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import axios from "axios";
@@ -19,21 +19,53 @@ import { ListItem, Text, Divider, Tooltip } from "react-native-elements";
 import { Card } from "@rneui/base";
 import { Colors } from "@/constants/Colors";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { DATA_API_ADDRESS } from "@/constants/Variables";
+import {
+  CATEGORIES_API_ADDRESS,
+  DATA_API_ADDRESS,
+  METHOD_API_ADDRESS,
+  TAGS_API_ADDRESS,
+  TYPES_API_ADDRESS,
+} from "@/constants/Variables";
 
 interface DataRowProps {
   exp: DataType;
   isExpanded: boolean;
   toggleExpand: (id: number) => void;
+  AssignMethod: (id: number) => string;
+  AssignType: (id: number) => string;
+  AssignCategory: (id: number) => string;
+  AssignTag: (id: number) => string;
 }
 type DataType = {
-  id: number,
-  amount: number,
-  time: Date,
-  description: string,
-  type_id: number,
-  tag_id: number,
-  method_id: number
+  id: number;
+  amount: number;
+  time: Date;
+  description: string;
+  type_id: number;
+  tag_id: number;
+  method_id: number;
+};
+
+type Tag = {
+  id: number;
+  tag_name: string;
+  category_id: number;
+  category_name: string;
+};
+
+type Type = {
+  id: number;
+  type_name: string;
+};
+
+type Method = {
+  id: number;
+  method_name: string;
+};
+
+type Category = {
+  id: number;
+  category_name: string;
 };
 
 //function to format date to our liking
@@ -45,16 +77,31 @@ const formatDate = (dateString: Date) => {
   return `${day}.${month}.${year}`;
 };
 
-if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
+if (
+  Platform.OS === "android" &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
   UIManager.setLayoutAnimationEnabledExperimental(true);
 }
 
 const DataRow: React.FC<DataRowProps> = memo(
-  ({ exp, isExpanded, toggleExpand }) => {
+  ({
+    exp,
+    isExpanded,
+    toggleExpand,
+    AssignMethod,
+    AssignCategory,
+    AssignType,
+    AssignTag,
+  }) => {
     return (
       <React.Fragment key={exp.id}>
         <ListItem
-          containerStyle={[styles.listItemContainer, styles.listItemSpacing, isExpanded && styles.listItemContainerExpanded]}
+          containerStyle={[
+            styles.listItemContainer,
+            styles.listItemSpacing,
+            isExpanded && styles.listItemContainerExpanded,
+          ]}
           onPress={() => toggleExpand(exp.id)}
         >
           <ListItem.Content>
@@ -69,7 +116,7 @@ const DataRow: React.FC<DataRowProps> = memo(
 
           {/* Dropdown Icon */}
           <MaterialCommunityIcons
-            name={isExpanded ? 'chevron-up' : 'chevron-down'}
+            name={isExpanded ? "chevron-up" : "chevron-down"}
             size={24}
             color={Colors.dark.primary}
           />
@@ -78,19 +125,39 @@ const DataRow: React.FC<DataRowProps> = memo(
         {/* Expanded Content */}
         {isExpanded && (
           <ListItem containerStyle={styles.expandedContent}>
-            <ListItem.Content style={{ flexDirection: "column" }}><Text style={styles.cellText}>
-              Tag Name: {exp.tag_id}
-            </Text>
-              <Text style={styles.cellText}>Method Name: {exp.method_id}</Text>
-              <Text style={styles.cellText}>Category Name: {exp.method_id}</Text>
-              <Text style={styles.cellText}>Type Name: {exp.type_id}</Text>
-              <ListItem.Content style={{ alignSelf: "flex-end", flexDirection: "row" }}>
-                  <MaterialCommunityIcons
-                    name="pencil"
-                    size={24}
-                    color={Colors.dark.primary}
-                    style={{ alignSelf: "flex-end", marginRight: 15 }}
-                  />
+            <ListItem.Content style={{ flexDirection: "column" }}>
+              <View style={styles.rowContainer}>
+                <Text style={styles.labelText}>Tag Name: </Text>
+                <Text style={styles.cellText}>{AssignTag(exp.tag_id)}</Text>
+              </View>
+
+              <View style={styles.rowContainer}>
+                <Text style={styles.labelText}>Method Name: </Text>
+                <Text style={styles.cellText}>
+                  {AssignMethod(exp.method_id)}
+                </Text>
+              </View>
+
+              <View style={styles.rowContainer}>
+                <Text style={styles.labelText}>Category Name: </Text>
+                <Text style={styles.cellText}>
+                  {AssignCategory(exp.tag_id)}
+                </Text>
+              </View>
+
+              <View style={styles.rowContainer}>
+                <Text style={styles.labelText}>Type Name: </Text>
+                <Text style={styles.cellText}>{AssignType(exp.type_id)}</Text>
+              </View>
+              <ListItem.Content
+                style={{ alignSelf: "flex-end", flexDirection: "row" }}
+              >
+                <MaterialCommunityIcons
+                  name="pencil"
+                  size={24}
+                  color={Colors.dark.primary}
+                  style={{ alignSelf: "flex-end", marginRight: 15 }}
+                />
                 <MaterialCommunityIcons
                   name="trash-can"
                   size={24}
@@ -105,11 +172,16 @@ const DataRow: React.FC<DataRowProps> = memo(
     );
   },
   (prevProps, nextProps) =>
-    prevProps.exp === nextProps.exp && prevProps.isExpanded === nextProps.isExpanded
+    prevProps.exp === nextProps.exp &&
+    prevProps.isExpanded === nextProps.isExpanded
 );
 
 const dashboard = () => {
   const [data, setData] = useState<DataType[] | null>([]);
+  const [tags, setTags] = useState<Tag[] | null>([]);
+  const [type, setType] = useState<Type[] | null>([]);
+  const [method, setMethod] = useState<Method[] | null>([]);
+  const [category, setCategory] = useState<Category[] | null>([]);
   const [screenWidth, setScreenWidth] = useState<ScaledSize>(
     Dimensions.get("window")
   );
@@ -132,26 +204,102 @@ const dashboard = () => {
         const response = await axios.get(DATA_API_ADDRESS);
         setData(response.data);
       } catch (err) {
-        console.error("Failed to fetch data: ", err);
+        console.error("Failed to fetch expenses: ", err);
+      }
+    };
+    const fetchTags = async () => {
+      try {
+        const response = await axios.get(TAGS_API_ADDRESS);
+        setTags(response.data);
+      } catch (err) {
+        console.error("Failed to fetch tags", err);
+      }
+    };
+    const fetchMethods = async () => {
+      try {
+        const response = await axios.get(METHOD_API_ADDRESS);
+        setMethod(response.data);
+      } catch (err) {
+        console.error("Failed to fetch methods", err);
+      }
+    };
+    const fetchTypes = async () => {
+      try {
+        const response = await axios.get(TYPES_API_ADDRESS);
+        setType(response.data);
+      } catch (err) {
+        console.error("Failed to fetch tags", err);
+      }
+    };
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get(CATEGORIES_API_ADDRESS);
+        setCategory(response.data);
+      } catch (err) {
+        console.error("Failed to fetch tags", err);
       }
     };
 
     fetchData();
+    fetchTags();
+    fetchMethods();
+    fetchTypes();
+    fetchCategories();
   }, []);
+
+  const AssignMethod = (id: number): string => {
+    const assignedMethod = method?.find((m) => m.id === id);
+    if (assignedMethod) {
+      return assignedMethod.method_name;
+    } else {
+      return "Unknown Method";
+    }
+  };
+
+  const AssignType = (id: number): string => {
+    const assignedType = type?.find((t) => t.id === id);
+    if (assignedType) {
+      return assignedType.type_name;
+    } else {
+      return "Unknown Type";
+    }
+  };
+
+  const AssignCategory = (id: number): string => {
+    const assignedTag = tags?.find((t) => t.id === id);
+    if (assignedTag) {
+      const assignedCategory = category?.find(
+        (c) => c.id === assignedTag.category_id
+      );
+      if (assignedCategory) {
+        return assignedCategory.category_name;
+      } else {
+        return "Unknown Category";
+      }
+    } else {
+      return "Unknown Tag for the Category";
+    }
+  };
+  const AssignTag = (id: number): string => {
+    const assignedTag = tags?.find((t) => t.id === id);
+    if (assignedTag) {
+      return assignedTag.tag_name;
+    } else {
+      return "Unknown Tag";
+    }
+  };
 
   const toggleExpand = useCallback((id: number) => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     setExpandedRow((prevExpandedRow) => (prevExpandedRow === id ? null : id));
   }, []);
 
-
-
   const keyExtractor = (exp: DataType) => exp.id.toString();
 
   return (
     <SafeAreaView style={styles.container}>
       <Card containerStyle={styles.cardStyle}>
-      <Card.Title style={styles.cardTitle}>Expense/Income</Card.Title>
+        <Card.Title style={styles.cardTitle}>Expense/Income</Card.Title>
         {/* Headers */}
         <ListItem containerStyle={styles.headerContainer}>
           <ListItem.Content>
@@ -165,11 +313,8 @@ const dashboard = () => {
               Description
             </ListItem.Title>
           </ListItem.Content>
-          <ListItem.Content>
-
-          </ListItem.Content>
+          <ListItem.Content></ListItem.Content>
         </ListItem>
-
       </Card>
 
       {/* Data */}
@@ -177,14 +322,22 @@ const dashboard = () => {
         data={data}
         keyExtractor={keyExtractor}
         renderItem={({ item }) => (
-          <DataRow exp={item} isExpanded={expandedRow === item.id} toggleExpand={toggleExpand} />
+          <DataRow
+            exp={item}
+            isExpanded={expandedRow === item.id}
+            toggleExpand={toggleExpand}
+            AssignCategory={AssignCategory}
+            AssignMethod={AssignMethod}
+            AssignTag={AssignTag}
+            AssignType={AssignType}
+          />
         )}
         // TODO: Change number on web
         initialNumToRender={10}
         maxToRenderPerBatch={5}
         windowSize={10}
         contentContainerStyle={styles.scrollView}
-      // ItemSeparatorComponent={() => <Divider />}
+        // ItemSeparatorComponent={() => <Divider />}
       />
     </SafeAreaView>
   );
@@ -196,10 +349,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.dark.background,
+    paddingHorizontal: 10,
   },
   scrollView: {
     flexGrow: 1,
-    paddingVertical: 16,
+    paddingVertical: 8,
   },
   view: {
     justifyContent: "center",
@@ -208,7 +362,7 @@ const styles = StyleSheet.create({
   cellText: {
     fontSize: 14,
     color: Colors.dark.text,
-    textAlign: "center",
+    flex: 2,
     // paddingVertical: 5,
   },
   headerText: {
@@ -224,7 +378,7 @@ const styles = StyleSheet.create({
     width: "100%",
     alignSelf: "center",
     backgroundColor: Colors.dark.background,
-    borderWidth: 0,  
+    borderWidth: 0,
     marginHorizontal: 0,
     paddingHorizontal: 0,
     paddingBottom: 5,
@@ -253,5 +407,17 @@ const styles = StyleSheet.create({
   listItemSpacing: {
     marginTop: 5,
   },
-
+  rowContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    marginBottom: 5,
+  },
+  labelText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: Colors.dark.text,
+    flex: 1,  
+    textAlign: 'right',  
+    paddingRight: 20,  
+  },
 });
--- constants/Variables.ts
+++ constants/Variables.ts
@@ -47,4 +47,16 @@ export const TABS_CONFIGURATION = [
     },
 
 ];
-export const DATA_API_ADDRESS = "http://51.20.113.113:4000/data"
+
+//#region API ENDPOINTS
+export const DATA_API_ADDRESS = "http://51.20.113.113:4000/finance-tracker/api/expenses"
+
+export const TAGS_API_ADDRESS = "http://51.20.113.113:4000/finance-tracker/api/tags"
+
+export const TYPES_API_ADDRESS = "http://51.20.113.113:4000/finance-tracker/api/types"
+
+export const METHOD_API_ADDRESS = "http://51.20.113.113:4000/finance-tracker/api/methods"
+
+export const CATEGORIES_API_ADDRESS = "http://51.20.113.113:4000/finance-tracker/api/categories"
+
+//#endregion
